f6ed60c1161ef4fd5f58af5ee4bb3de3172d529b
Maiko Duarte <mkdu185@gmail.com>
2023-09-23 12:45:44 -0300

    "start: Initial commit"

---
+++ src/test/java/engineer/maiko/desafiodiodesignpatterns/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.maiko.desafiodiodesignpatterns.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

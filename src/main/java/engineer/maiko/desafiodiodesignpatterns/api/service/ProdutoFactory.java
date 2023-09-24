778df3d8dfa1312e85e696b05b518a81118c80d1
Maiko Duarte <mkdu185@gmail.com>
2023-09-24 08:46:51 -0300

    feat(service): Created ProdutoFactory for product creation.

---
+++ src/main/java/engineer/maiko/desafiodiodesignpatterns/api/service/ProdutoFactory.java
@@ -0,0 +1,8 @@
+package engineer.maiko.desafiodiodesignpatterns.api.service;
+
+
+import engineer.maiko.desafiodiodesignpatterns.api.model.Produto;
+
+public interface ProdutoFactory {
+    Produto criarProduto();
+}

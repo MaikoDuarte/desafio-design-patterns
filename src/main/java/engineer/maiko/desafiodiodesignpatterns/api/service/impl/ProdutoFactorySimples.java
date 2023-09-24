4677e0ce05295fc9413ea50feda24507800e3958
Maiko Duarte <mkdu185@gmail.com>
2023-09-24 08:48:33 -0300

    feat(service/impl): Created ProdutoFactorySimples for simple product creation.

---
+++ src/main/java/engineer/maiko/desafiodiodesignpatterns/api/service/impl/ProdutoFactorySimples.java
@@ -0,0 +1,15 @@
+package engineer.maiko.desafiodiodesignpatterns.api.service.impl;
+
+import engineer.maiko.desafiodiodesignpatterns.api.model.Produto;
+import engineer.maiko.desafiodiodesignpatterns.api.service.ProdutoFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoFactorySimples implements ProdutoFactory {
+
+    @Override
+    public Produto criarProduto() {
+
+        return new Produto();
+    }
+}

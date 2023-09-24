d6aa20ba67516563aca4a0c62fbb82e95c4d5ac5
Maiko Duarte <mkdu185@gmail.com>
2023-09-24 08:47:48 -0300

    feat(service/impl): Added EstrategiaGerenciamentoEstoqueLote for batch inventory strategy

---
+++ src/main/java/engineer/maiko/desafiodiodesignpatterns/api/service/impl/EstrategiaGerenciamentoEstoqueLote.java
@@ -0,0 +1,16 @@
+package engineer.maiko.desafiodiodesignpatterns.api.service.impl;
+
+import engineer.maiko.desafiodiodesignpatterns.api.model.Produto;
+import engineer.maiko.desafiodiodesignpatterns.api.service.EstrategiaGerenciamentoEstoque;
+import org.springframework.stereotype.Service;
+
+;
+
+@Service
+public class EstrategiaGerenciamentoEstoqueLote implements EstrategiaGerenciamentoEstoque {
+
+    @Override
+    public void gerenciarEstoque(Produto produto, int quantidade) {
+
+    }
+}

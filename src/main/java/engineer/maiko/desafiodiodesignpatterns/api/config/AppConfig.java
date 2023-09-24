198fb953298cdd8e97bd99e90e5b3487e4fb9590
Maiko Duarte <mkdu185@gmail.com>
2023-09-24 08:42:30 -0300

    feat(config): Added AppConfig for application configuration.

---
+++ src/main/java/engineer/maiko/desafiodiodesignpatterns/api/config/AppConfig.java
@@ -0,0 +1,21 @@
+package engineer.maiko.desafiodiodesignpatterns.api.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
+}

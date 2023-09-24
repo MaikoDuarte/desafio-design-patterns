aa4275e72fb047f1d061f00f9afa507ba7c946a8
Maiko Duarte <mkdu185@gmail.com>
2023-09-24 08:47:09 -0300

    feat(service/impl): Implemented EstoqueComControleDeLoteServiceImpl for inventory with batch control.

---
+++ src/main/java/engineer/maiko/desafiodiodesignpatterns/api/service/impl/EstoqueComControleDeLoteServiceImpl.java
@@ -0,0 +1,58 @@
+package engineer.maiko.desafiodiodesignpatterns.api.service.impl;
+
+import engineer.maiko.desafiodiodesignpatterns.api.exception.EstoqueException;
+import engineer.maiko.desafiodiodesignpatterns.api.model.Produto;
+import engineer.maiko.desafiodiodesignpatterns.api.repository.ProdutoRepository;
+import engineer.maiko.desafiodiodesignpatterns.api.service.EstoqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Primary
+public class EstoqueComControleDeLoteServiceImpl implements EstoqueService {
+    private final ProdutoRepository produtoRepository;
+
+    @Autowired
+    public EstoqueComControleDeLoteServiceImpl(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @Override
+    public List<Produto> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    @Override
+    public Produto encontrarProdutoPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Produto adicionarProduto(Produto produto) {
+        return null;
+    }
+
+    @Override
+    public Produto atualizarProduto(Long id, Produto produto) {
+        return null;
+    }
+
+    @Override
+    public void removerProduto(Long id) {
+
+    }
+
+    @Override
+    public Produto adicionarAoEstoque(Long id, int quantidade) throws EstoqueException {
+        return null;
+    }
+
+    @Override
+    public Produto removerDoEstoque(Long id, int quantidade) throws EstoqueException {
+        return null;
+    }
+
+}
